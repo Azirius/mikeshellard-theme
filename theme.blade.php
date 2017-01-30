9f46e8b740068ad597712f6a5923c10885a4f84c
Mike Shellard <contact@mikeshellard.me>
2017-01-30 11:09:06 +0000

    initial

---
+++ theme.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
+    <title>Design</title>
+
+    <!-- Theme CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.3.1/css/bulma.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+</head>
+
+<body>
+
+    <section class="hero is-primary is-small is-info">
+        <div class="hero-head">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title"><a href="/">MikeShellard.me</a></h1>
+                </div>
+            </div>
+
+            <div class="hero-foot">
+                <nav class="tabs is-boxed is-fullwidth">
+                    <div class="container">
+                        <ul>
+                            <li class="is-active"><a href="#">Home</a></li>
+                            <li><a href="#">About Me</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </section>
+
+    <section class="section">
+        <div class="container">
+            <div class="heading">
+                <h1 class="title">Hello World</h1>
+                <h2 class="subtitle">Posted Today!</h2>
+            </div>
+            <hr>
+            <h1 class="subtitle">Hello World</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, minus laboriosam recusandae quis fugiat atque doloribus temporibus alias officiis! Animi, culpa, molestias. Hic non magnam eum laborum, id, nulla eveniet?
+            </p>
+        </div>
+    </section>
+
+
+    <footer class="footer">
+        <div class="container">
+            <div class="content has-text-centered">
+                <p>
+                    <a href="https://twitter.com/Azirius" target="_blank">
+                        <span class="fa-stack fa-lg">
+                            <i class="fa fa-circle fa-stack-2x"></i>
+                            <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                        </span>
+                    </a>
+                    <a href="https://www.facebook.com/Azirius" target="_blank">
+                        <span class="fa-stack fa-lg">
+                            <i class="fa fa-circle fa-stack-2x"></i>
+                            <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                        </span>
+                    </a>
+                    <a href="https://github.com/Azirius" target="_blank">
+                        <span class="fa-stack fa-lg">
+                            <i class="fa fa-circle fa-stack-2x"></i>
+                            <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                        </span>
+                    </a>
+                </p>
+                
+                <p class="copyright text-muted">Copyright &copy; Mike Shellard 2016</p>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
